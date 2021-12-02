cfad472717097d3131286a3e348aa7d691c92c48
Eero Tanskanen <eero.tanskanen@haaveinc.com>
2021-12-02 10:49:20 +0000

    Day 1 part 2.

--- 1.ts
+++ 1.ts
@@ -7,10 +7,37 @@ const inputArray = oneJson.text |>> textRowsToArray |> #.map(stringToNumber);
 const sampleInputArray = oneSampleJson.text |>> textRowsToArray |> #.map(stringToNumber);
 
 const calculateDepthIncreases = (depths: Array<number>) => (depths
-    .reduce<[number, number]>(([lastDepth, depthIncreaseCount], depth) => [
+    .reduce<[lastDepth: number, depthIncreaseCount: number]>(([lastDepth, depthIncreaseCount], depth) => [
         depth,
         depthIncreaseCount + ((depth > lastDepth) ? 1 : 0)
     ], [Number.MAX_VALUE, 0]))[1];
 
 (`sample: Depth increased ${calculateDepthIncreases(sampleInputArray)} times`) |>> console.log;
 (`input: Depth increased ${calculateDepthIncreases(inputArray)} times`) |>> console.log;
+
+const depthSum = ([a, b, c]: [a: number, b: number, c: number]) => a + b + c;
+
+sampleInputArray
+    .map((depth, i, a) =>
+        (i + 2) < a.length
+            ? depthSum(a.slice(i, i + 3) as [number, number, number])
+            : undefined
+    )
+    .filter((a): a is number => a !== undefined)
+    |>> calculateDepthIncreases
+    |> `averaged sample: Depth increased ${#} times` 
+    |>> console.log;
+
+inputArray
+    .map((depth, i, a) =>
+        (i + 2) < a.length
+            ? depthSum(a.slice(i, i + 3) as [number, number, number])
+            : undefined
+    )
+    .filter((a): a is number => a !== undefined)
+    |>> calculateDepthIncreases
+    |> `averaged sample: Depth increased ${#} times` 
+    |>> console.log;
+
+
+// console.log(sampleInputArray);
