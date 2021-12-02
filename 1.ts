b03e07e8a0e809c5c6b2ee3f832e382cb1333d9b
Eero Tanskanen <pokute@draconus.com>
2021-12-02 08:50:24 +0000

    Solution for day 1 part 1. Also env setups.

--- 1.ts
+++ 1.ts
@@ -1,9 +1,16 @@
 // import { text as day1Text } from './inputs/1.json' assert { type: 'json' };
-import * as oneJson from './inputs/1.json' assert { type: 'json' };
+import oneJson from './inputs/1.json' assert { type: 'json' };
+import oneSampleJson from './inputs/1sample.json' assert { type: 'json' };
+import { stringToNumber, textRowsToArray } from './inputUtils.js';
 
-console.log('Hmm...');
-console.log(oneJson.default);
-console.log('Hmm...');
+const inputArray = oneJson.text |>> textRowsToArray |> #.map(stringToNumber);
+const sampleInputArray = oneSampleJson.text |>> textRowsToArray |> #.map(stringToNumber);
 
-oneJson |> console.log;
-//'test' |>> console.log;
+const calculateDepthIncreases = (depths: Array<number>) => (depths
+    .reduce<[number, number]>(([lastDepth, depthIncreaseCount], depth) => [
+        depth,
+        depthIncreaseCount + ((depth > lastDepth) ? 1 : 0)
+    ], [Number.MAX_VALUE, 0]))[1];
+
+(`sample: Depth increased ${calculateDepthIncreases(sampleInputArray)} times`) |>> console.log;
+(`input: Depth increased ${calculateDepthIncreases(inputArray)} times`) |>> console.log;
--- inputUtils.ts
+++ inputUtils.ts
@@ -1,2 +1,4 @@
 export const textRowsToArray = (text: string): Array<string> =>
     text.split('\n');
+
+export const stringToNumber = (entry: string) => Number(entry);
